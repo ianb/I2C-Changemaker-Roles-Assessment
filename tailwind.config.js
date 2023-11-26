8424f71464796c53bf7f186d14d8b480cc221d1c
Ian Bicking <ianb@colorstudy.com>
2023-11-26 00:01:56 -0600

    More style improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+const colors = require("tailwindcss/colors");
+
+// Avoid warnings about deprecated colors:
+const simpleColors = Object.assign({}, colors);
+delete simpleColors.coolGray;
+delete simpleColors.trueGray;
+delete simpleColors.warmGray;
+delete simpleColors.blueGray;
+delete simpleColors.lightBlue;
+
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: Object.assign({}, simpleColors, {
+        inspireOrange: "#f24b0f",
+        inspireYellow: "#ffbb0d",
+        inspireTrueOrange: "#f1861a",
+      }),
+    },
   },
   plugins: [],
 }
