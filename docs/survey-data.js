e835e5d883ee80dc2181e44b19724e4fb89fdf1b
inspiretochange <155693478+inspiretochange@users.noreply.github.com>
2024-02-07 11:19:46 -0600

    Update survey-data.js

--- docs/survey-data.js
+++ docs/survey-data.js
@@ -19,93 +19,93 @@ window.orderedData = [
     version: 1,
     questions: parseData(`
       I seek opportunities to engage in public forums and discussions.
-I actively work towards changes in policies and legislation
-I mobilize resources for causes.
-I rally public support for causes.
-I facilitate foster understanding between divided people or groups.
-I create connections between individuals or groups who do not naturally engage with each other.
-I help build trust across different social divides
-I help divided people or groups work together.
-I translate visionary ideas into actionable plans.
-I make sure the details are covered.
-I methodically develop and document systems to bring ideas into reality.
-I work to improve efficiency and sustainability is systems.
-I provide emotional support to those in need.
-I care for the well-being of my community.
-I offer empathy and compassion as a stragegy for change.
-I create safe spaces for people and groups to experience personal growth.
-I inspire others to take action towards social change.
-I generate momentum to amplify or accelerate the pace of change.
-I navigate obstacles that might slow down the pace of change.
-I create a sense of urgency for change.
-I express my vision through art, literature, or technology.
-I produce art or cultural pieces that communicate perspectives, emotions, and insights.
-I produce art or cultural pieces that offer new ways for others to see the world.
-I produce art or cultural pieces because I feel driven to do so.
-I directly challenge established norms and systems.
-I work towards radical solutions to induce significant changes.
-I disrupt the status quo with innovative products or business models.
-I actively drive change, even when it makes other uncomfortable
-I support others through significant transitions, whether welcomed or unwelcome,
-I help people see both the gains and losses in major transitions
-I help people recognize, experience, and honor complex feelings during significant transitions
-I help people adopt proactive coping strategies to increase their resilience when navigating transitions
-I critically analyze moral implications of actions and decisions.
-I advocate for ethical principles in my personal and professional spheres.
-I help others develop guidelines to navigate ethical dilemmas.
-I assist individuals and organizations in holding themselves accountable to high ethical standards.
-I provide direction and advice based on my wisdom and experience.
-I help others navigate their paths with insight and foresight.
-I mentor individuals seeking personal or professional growth.
-I share my knowledge to empower and enlighten others who are seeking guidance.
-I facilitate healing processes for physical, emotional, mental, and spiritual well-being.
-I use holistic approaches to restore balance and harmony.
-I provide a compassionate presence and listening ear to those in pain.
-I integrate traditional and modern healing techniques to address individual and collective needs.
-I create environments where everyone feels valued and included.
-I facilitate gatherings that encourage connection and community.
-I provide hospitality that makes people feel at home.
-I proactively address potential issues to maintain a sense of community and belonging.
-I use humor to highlight social and political issues.
-I provoke thought through satire and comedic critique.
-I challenge authority and norms with wit and irony.
-I make complex ideas accessible and entertaining through humor.
-I preserve cultural heritage and traditions for future generations.
-I protect resources and diversity through conservation efforts.
-I maintain artifacts, teachings, and practices that hold historical significance.
-I advocate for policies and practices that ensure the longevity of precious assets.
-I use spiritual insight and practices to support individual and collective change.
-I offer support to those seeking to deepen their spiritual understanding and connection.
-I seek the knowledge and understanding of spiritual traditions, teachings, and philosophies.
-I create spaces for open dialogue about spiritual experiences, doubts, and insights.
-I facilitate dialogue to find common ground among disputing parties.
-I employ diplomacy to resolve conflicts and reach agreements.
-I balance competing interests to achieve mutually beneficial outcomes.
-I advocate for fairness and equity in negotiations when making decisions.
-I identify trends and correlations within complex data sets.
-I anticipate future developments by analyzing current patterns.
-I inform strategic decisions by envisioning multiple possible strategic pathways.
-I leverage patterns to predict and adapt to changing environments.
-I design comprehensive strategies to achieve long-term goals.
-I organize resources and schedules for optimal efficiency.
-I anticipate challenges and develop contingency plans.
-I facilitate the execution of complex projects.
-I handle crises effectively.
-I collaborate with teams to ensure coordinated responses to immediate crises.
-I assess situations rapidly and decide on the best course of action.
-I help calm and reassure victims and their families in a crisis.
-I weave narratives that connect with people on an emotional level.
-I inspire action and change through the power of narrative.
-I utilize the power of stories to evoke strong emotional responses and motivate change.
-I share stories as direct calls to action.
-I envision futures that transcend current limitations.
-I inspire others to believe in and work towards a future possibility.
-I lead by example, demonstrating faith in what’s possible.
-I communicate future possibilities in a way that motivates and galvanizes support.
-I connect individuals and communities to build stronger networks.
-I integrate diverse elements into a cohesive whole.
-I enrich the social fabric by deepening inclusivity and mutual support.
-I craft strategies that reinforce community resilience and solidarity.
+Actively work towards changes in policies and legislation
+Mobilize resources for causes.
+Rally public support for causes.
+Foster understanding between divided people or groups.
+Create connections between individuals or groups who do not naturally engage with each other.
+Build trust across different social divides
+Help divided people or groups work together.
+Translate visionary ideas into actionable plans.
+Make sure the details are considered and taken care of.
+Methodically develop and document systems to bring ideas into reality.
+Improve efficiency and sustainability is systems.
+Provide emotional support to those in need.
+Care for the well-being of my community.
+Offer empathy and compassion as a stragegy for change.
+Create safe spaces for people and groups to experience personal growth.
+Inspire others to take action towards social change.
+Generate momentum to amplify or accelerate the pace of change.
+Navigate obstacles that might slow down the pace of change.
+Create a sense of urgency for change.
+Express my vision through art, literature, or technology.
+Produce art or cultural pieces that communicate perspectives, emotions, and insights.
+Produce art or cultural pieces that offer new ways for others to see the world.
+Produce art or cultural pieces because I feel driven to do so.
+Directly challenge people who hold established norms and systems in place.
+Generate radical solutions to for significant change.
+Disrupt the status quo with innovative products or business models.
+Drive change, even when it makes other uncomfortable
+Support others through significant transitions, whether welcomed or unwelcome,
+Help people see both the gains and losses in major transitions
+Help people recognize, experience, and honor complex feelings during significant transitions
+Help people adopt proactive coping strategies to increase their resilience when navigating transitions
+Critically analyze moral implications of actions and decisions.
+Advocate for ethical principles in my personal and professional spheres.
+Help others develop guidelines to navigate ethical dilemmas.
+Assist individuals and organizations in holding themselves accountable to high ethical standards.
+Provide direction and advice based on my wisdom and experience.
+Help others navigate their paths with insight and foresight.
+Mentor individuals seeking personal or professional growth.
+Share my knowledge to empower and enlighten others who are seeking guidance.
+Facilitate healing processes for physical, emotional, mental, and spiritual well-being.
+Use holistic approaches to restore balance and harmony.
+Provide a compassionate presence and listening ear to those in pain.
+Integrate traditional and modern healing techniques to address individual and collective needs.
+Create environments where everyone feels valued and included.
+Facilitate gatherings that encourage connection and community.
+Provide hospitality that makes people feel at home.
+Proactively address potential issues to maintain a sense of community and belonging.
+Use humor to highlight social and political issues.
+Use satire and comedic critique to provoke engagement.
+Use comedy to challenge authority and norms.
+Use humor to make complex ideas accessible.
+Preserve cultural heritage and traditions for future generations.
+Protect resources and diversity through conservation efforts.
+Maintain artifacts, teachings, and practices that hold cultural or historical significance.
+Work towards the care and protection of precious assets.
+Use spiritual insight and practices to support individual and collective change.
+Offer support to those seeking to deepen their spiritual understanding and connection.
+Seek the knowledge and understanding of spiritual traditions, teachings, and philosophies.
+Create spaces for open dialogue about spiritual experiences, doubts, and insights.
+Facilitate dialogue to find common ground among disputing parties.
+Employ diplomacy to resolve conflicts.
+Balance competing interests to resolve a disagreement.
+Help people equitably realize mutually beneficial outcomes.
+Identify trends and correlations within complex data sets.
+Anticipate future possibilities by analyzing current patterns.
+Envisioning multiple possible strategic pathways towards change.
+Use pattern-spotting to predict and adapt to changing environments.
+Design comprehensive strategies to get us from "point a" to "point b".
+Organize resources and schedules for optimal efficiency.
+Ensure everyone knows what role they play in effectively executing a plan.
+Plan for effective resource use in the short- and long-term.
+Handle urgent and emergent crises effectively.
+Coordinate responses to urgent and emergent crises.
+Assess situations rapidly and decide on the best course of action.
+Calm and reassure victims and their families in a crisis.
+Generate narratives that connect with people on an emotional level.
+Inspire action through the power of story.
+Utilize the power of stories to change people's hearts and minds.
+Share stories as direct "calls to action".
+Create visions of futures that transcend current limitations.
+Inspire others to believe in and work towards a better future
+Lead by example, demonstrating faith in what’s possible.
+Motivate and galvanize support towards a vision.
+Connect individuals and communities to build stronger networks.
+Integrate diverse elements into a cohesive whole.
+Enrich the social fabric by deepening inclusivity and mutual support.
+Facilitate engagements that reinforce community resilience and solidarity.
     `)
   }
 ];
